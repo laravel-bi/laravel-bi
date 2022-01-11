2283330b559d40b79b1c72e1786b4e2269a5752d
Alberto Bottarini <alberto.bottarini@gmail.com>
2022-01-11 21:30:31 +0100

    remove console.log

--- resources/js/components/mixins/toasts.js
+++ resources/js/components/mixins/toasts.js
@@ -3,7 +3,6 @@ import EventBus from "../../utils/EventBus";
 export default {
     methods: {
         sendToast(message, error = true) {
-            console.log('sendToast', message);
             EventBus.$emit('toast', {
                 message,
                 error,
